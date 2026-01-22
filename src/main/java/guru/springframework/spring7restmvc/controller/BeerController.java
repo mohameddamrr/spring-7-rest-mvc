3702c755dd753698124207c2ab10b3865f7d121f
mohameddamrr <mohmd.amr@hotmail.com>
2026-01-22 11:23:03 +0200

    added a function list beers in service and controller and tested it usuing postman

--- src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
@@ -1,11 +1,27 @@
 package guru.springframework.spring7restmvc.controller;
 
+import guru.springframework.spring7restmvc.model.Beer;
 import guru.springframework.spring7restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
+
+    public Beer getBeerById(UUID id) {
+
+        return beerService.getBeerById(id);
+    }
 }
--- src/main/java/guru/springframework/spring7restmvc/service/BeerService.java
+++ src/main/java/guru/springframework/spring7restmvc/service/BeerService.java
@@ -2,9 +2,12 @@ package guru.springframework.spring7restmvc.service;
 
 import guru.springframework.spring7restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }
