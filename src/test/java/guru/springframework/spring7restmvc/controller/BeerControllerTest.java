46dbc653aa9eeb5f2bfed6ede900025fa4d641e7
mohameddamrr <mohmd.amr@hotmail.com>
2026-01-26 14:20:19 +0200

    added update beer and used verify instead of given as update in service is a void function also used the objectmapper to serialize the beer object we want to update to json

--- src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring7restmvc.controller;
 
 import guru.springframework.spring7restmvc.model.Beer;
+import guru.springframework.spring7restmvc.model.BeerStyle;
 import guru.springframework.spring7restmvc.service.BeerService;
 
 import guru.springframework.spring7restmvc.service.BeerServiceImpl;
@@ -9,18 +10,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 
+
+
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -42,6 +48,26 @@ class BeerControllerTest {
     @BeforeEach
     void setUp() {
         beerServiceImpl = new BeerServiceImpl();
+    }
+
+
+    @Test
+    void updateBeer() throws Exception {
+        Beer beer = beerServiceImpl.listBeers().get(0);
+        beer.setBeerStyle(BeerStyle.IPA);
+        beer.setBeerName("Updated");
+        beer.setPrice(BigDecimal.valueOf(12.99));
+        beer.setUpc("88");
+        beer.setQuantityOnHand(77);
+
+//        given(beerService.updateBeerById(beer.getId(),beer)); as it doesnt return
+
+        mockMvc.perform(put("/api/v1/beer/" + beer.getId()).accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beer)))
+                ;
+
+        verify(beerService).updateBeerById(any(UUID.class),any(Beer.class)); //we use this instead of given as it doenst return
     }
 
 
