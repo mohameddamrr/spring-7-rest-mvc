2669519d2d046fe729719a45de342f2e3029308d
mohameddamrr <mohmd.amr@hotmail.com>
2026-01-27 11:18:52 +0200

    adding beer jpda entity sam as dto but just used @getter and @setter instead of data and added @Entity and @Id

---
+++ src/main/java/guru/springframework/spring7restmvc/entities/Beer.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring7restmvc.entities;
+
+import guru.springframework.spring7restmvc.model.BeerStyle;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter //as not recommended to use @data for jpa
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Beer {
+    @Id
+    private UUID id;
+    @Version
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
