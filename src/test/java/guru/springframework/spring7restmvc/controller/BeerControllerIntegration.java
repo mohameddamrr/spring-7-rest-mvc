e84ae9f8cae33ef96fc0ee84e48d1ab3cafa08af
mohameddamrr <mohmd.amr@hotmail.com>
2026-01-29 11:56:27 +0200

    tested getby id integraion test using springboottest annotation ,tested when throwing exception for not found id and when finding id

--- src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIntegration.java
+++ src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIntegration.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring7restmvc.controller;
 
+import guru.springframework.spring7restmvc.entities.Beer;
 import guru.springframework.spring7restmvc.mappers.BeerMapper;
 import guru.springframework.spring7restmvc.model.BeerDTO;
 import guru.springframework.spring7restmvc.repositories.BeerRepository;
@@ -11,7 +12,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -22,6 +25,22 @@ class BeerControllerIntegration {
     BeerController beerController;
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+    }
 
     @Test
     void testListBeers() {
